94df8fb7d7b1452af3271aebf3c558e49ae76e5b
diegojimenez77 <djpcoding8652@gmail.com>
2026-05-16 12:15:31 -0600

    adding Onboarding screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import "../global.css";
 
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -25,5 +26,9 @@ export default function RootLayout() {
     return null;
   }
 
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaProvider>
+  );
 }
---
+++ constants/images.ts
@@ -0,0 +1,17 @@
+const mascotLogo = require("../assets/assets/images/moscot-logo.png");
+const mascotWelcome = require("../assets/assets/images/mascot-welcome.png");
+const mascotAuth = require("../assets/assets/images/mascot-auth.png");
+const earth = require("../assets/assets/images/earth.png");
+const palace = require("../assets/assets/images/palace.png");
+const treasure = require("../assets/assets/images/treasure.png");
+const streakFire = require("../assets/assets/images/streak-fire.png");
+
+export const images = {
+  mascotLogo,
+  mascotWelcome,
+  mascotAuth,
+  earth,
+  palace,
+  treasure,
+  streakFire,
+};
